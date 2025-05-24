cf1fb9ad5a553bb4e7e2d765f90482aae15ca43f
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-05-23 22:44:09 -0300

    feat: create gamepad system

--- src/core/scenes/GameScene.ts
+++ src/core/scenes/GameScene.ts
@@ -8,26 +8,20 @@ import {
   InputComponent,
 } from "kanji-ecs/components";
 
-import { CameraComponent } from "../components/CameraComponent";
-
 import { MovementSystem } from "../systems/MovementSystem";
 import { KeyboardSystem } from "../systems/KeyboardSystem";
-import { CameraSystem } from "../systems/Camera/CameraSystem";
-import { CameraZoomSystem } from "../systems/Camera/CameraZoomSystem";
-import { CameraDragNDropSystem } from "../systems/Camera/CameraDragNDropSystem";
 
 import * as Utils from "../utils";
 import { PhysicsComponent } from "../components/PhysicsComponent";
 import { PhysicsSystem } from "../systems/PhysicsSystem";
+import { GamepadSystem } from "../systems/GamepadSystem";
 
 export class GameScene extends Phaser.Scene {
   player!: Entity;
   movementSystem!: MovementSystem;
   keyboardSystem!: KeyboardSystem;
-  cameraSystem!: CameraSystem;
-  cameraZoomSystem!: CameraZoomSystem;
-  cameraDragNDropSystem!: CameraDragNDropSystem;
   physicsSystem!: PhysicsSystem;
+  gamepadSystem!: GamepadSystem;
 
   constructor() {
     super("game-scene");
@@ -53,7 +47,6 @@ export class GameScene extends Phaser.Scene {
     const positionComponent = new PositionComponent(200, 200);
     const velocityComponent = new VelocityComponent(400);
     const inputComponent = new InputComponent();
-    const cameraComponent = new CameraComponent();
     const physicsComponent = new PhysicsComponent(
       sprite,
       positionComponent.x,
@@ -64,22 +57,19 @@ export class GameScene extends Phaser.Scene {
     this.player.add("velocity", velocityComponent);
     this.player.add("position", positionComponent);
     this.player.add("input", inputComponent);
-    this.player.add("camera", cameraComponent);
     this.player.add("physics", physicsComponent);
 
     // Systems
     this.movementSystem = new MovementSystem();
     this.keyboardSystem = new KeyboardSystem(this);
-    this.cameraSystem = new CameraSystem(this, this.player);
-    this.cameraZoomSystem = new CameraZoomSystem(this, this.player);
-    this.cameraDragNDropSystem = new CameraDragNDropSystem(this, this.player);
+    this.gamepadSystem = new GamepadSystem(this);
     this.physicsSystem = new PhysicsSystem();
   }
 
   update(_time: number) {
     this.movementSystem.update([this.player]);
     this.keyboardSystem.update([this.player]);
+    this.gamepadSystem.update([this.player]);
     this.physicsSystem.update([this.player]);
-    this.cameraSystem.update();
   }
 }
